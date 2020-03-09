cf9b573228b5e44af647e3428d3949c583284e36
theirish81 <theirish81@gmail.com>
2020-03-09 12:22:51 +0100

    externalizing configuration variables with env

--- docker-compose/init_db.sh
+++
@@ -1,3 +0,0 @@
-sudo docker cp afthem-dashboard.sql mariadb:afthem-dashboard.sql
-sudo docker cp mysql.ini mariadb:mysql.ini
-sudo docker exec mariadb sh -c 'mysql --defaults-extra-file=mysql.ini afthem-dashboard < afthem-dashboard.sql'
