8821200383b5ba45a907004ce8528cb6ed8224e1
Lorenzo Fontana <gufoscuro@gmail.com>
2020-04-01 11:13:23 +0200

    commit issue (fixed)

--- services/fs-service.js
+++ services/fs-service.js
@@ -110,7 +110,8 @@ module.exports.gitStatus = (dir_path) => {
                 results[filepath] = {
                     path: filepath,
                     status: status,
-                    staged: isStaged
+                    staged: isStaged,
+                    values: it
                 }
             })
             resolve (results)
@@ -120,16 +121,11 @@ module.exports.gitStatus = (dir_path) => {
 
 module.exports.commitAndPush = (opts) => {
     return new Promise ((resolve, reject) => {
-        // repoPath: dir,
-        // authorName: usr.firstName + ' ' + usr.lastName,
-        // authorUsername: usr.gitUsername,
-        // authorPassword: usr.gitPassword,
-        // message: mex !== '' ? mex : 'commit changes @ ' + new Date().toLocaleString()
-            
         var dir = path.join (process.cwd (), opts.repoPath);
         git.statusMatrix ({ fs, dir: dir }).then (files => {
+            // f[1] === 0 && f[2] === 2 && f[3] === 0
             Promise.all (
-                files.filter ((f) => { return (f[1] === 0 && f[2] === 2 && f[3] === 0) })
+                files.filter ((f) => { return (f[2] > f[3]) })
                     .map ((f) => git.add ({ fs, dir: dir, filepath: f[0] }))
             ).then ((r) => {
                 git.commit ({
